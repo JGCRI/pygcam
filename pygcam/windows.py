eb55954630d7cf3bcaecf82636bebdc3854cc236
Richard Plevin <rich@plevin.com>
2016-03-09 12:32:48 -0800

    no message

--- pygcam/windows.py
+++ pygcam/windows.py
@@ -1,16 +1,18 @@
 '''
   .. Stuff specific to MS Windows
 '''
-
-import os, ctypes, platform
-
-def IsSymlink(path):
-    FILE_ATTRIBUTE_REPARSE_POINT = 0x0400
-
-    return os.path.isdir(path) and \
-        (ctypes.windll.kernel32.GetFileAttributesW(unicode(path)) & FILE_ATTRIBUTE_REPARSE_POINT)
-
+import platform
 
 if platform.system() == 'Windows':
-    # Replace broken function with the one above
-    os.path.islink = IsSymlink
+
+    import os, ctypes
+
+    def islink(path):
+        FILE_ATTRIBUTE_REPARSE_POINT = 0x0400
+
+        return bool(os.path.isdir(path) and \
+            (ctypes.windll.kernel32.GetFileAttributesW(unicode(path)) & FILE_ATTRIBUTE_REPARSE_POINT))
+
+        # Replace broken function with the one above
+        # (In python 2.7.11 os.path.islink() indeed failed to detect link made with mklink)
+        os.path.islink = islink
