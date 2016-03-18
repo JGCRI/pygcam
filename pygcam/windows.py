8cbe086c6ddd29c3eeb483c75e34d3cb35b64f33
Richard Plevin <rich@plevin.com>
2016-03-17 17:30:18 -0700

    no message

--- pygcam/windows.py
+++ pygcam/windows.py
@@ -7,7 +7,7 @@ if platform.system() == 'Windows':
 
     import ctypes
 
-    def islink(path):
+    def islinkWindows(path):
         FILE_ATTRIBUTE_REPARSE_POINT = 0x0400
 
         return bool(os.path.isdir(path) and \
@@ -15,16 +15,16 @@ if platform.system() == 'Windows':
 
     # Replace broken function with the one above
     # (In python 2.7.11 os.path.islink() indeed failed to detect link made with mklink)
-    os.path.islink = islink
+    os.path.islink = islinkWindows
 
     # From http://stackoverflow.com/questions/6260149/os-symlink-support-in-windows
-    def symlink(src, dst):
+    def symlinkWindows(src, dst):
         csl = ctypes.windll.kernel32.CreateSymbolicLinkW
         csl.argtypes = (ctypes.c_wchar_p, ctypes.c_wchar_p, ctypes.c_uint32)
         csl.restype = ctypes.c_ubyte
         src = src.replace('/', '\\')
         flags = 1 if os.path.isdir(src) else 0
-        
+
         try:
             if csl(dst, src, flags) == 0:
                 raise ctypes.WinError()
@@ -32,4 +32,4 @@ if platform.system() == 'Windows':
             print "Failed to create symlink '%s' to '%s'" % (dst, src)
             pass
 
-    os.symlink = symlink
+    os.symlink = symlinkWindows
