db720a07ceda6fba8152088cefd7f051f27e88ea
Richard Plevin <rich@plevin.com>
2016-03-21 11:30:04 -0700

    Modified to use it own directory, avoiding hardcoding the path.

--- pygcam/bin/gt.cmd
+++ pygcam/bin/gt.cmd
@@ -1,1 +1,8 @@
-@python C:\Users\rjp\bitbucket\pygcam\pygcam\bin\gcamtool.py %*
+@ECHO OFF
+
+REM This script can be used under cygwin bash, which otherwise
+REM the path passed to python may be /cygwin/c/... which the
+REM Anaconda Python doesn't understand. 
+REM Note: %~dp0 is the directory this script lives in.
+
+python %~dp0\gcamtool.py %*
