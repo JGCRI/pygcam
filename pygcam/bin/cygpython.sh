accab132602783a09fdb5a9008137484746110d4
Richard Plevin <rich@plevin.com>
2016-03-24 18:31:31 -0700

    no message

--- pygcam/bin/cygpython.sh
+++ pygcam/bin/cygpython.sh
@@ -3,19 +3,23 @@
 # Use this script instead of python in shebang (#!) lines to fix cygwin paths
 # so a non-cygwin python can understand them.
 #
-if [[ $# != 1 ]]; then
-    echo "USAGE: pythonCygwin <filename>"
+if [[ $# == 0 ]]; then
+    echo "USAGE: pythonCygwin <filename> args..."
     exit -1
 fi
+
+script=$1
+shift
 
 path=$(which cygpath)
 
 if [[ $? == 0 ]]; then
     # if cygpath was found, convert the path
-    PythonFile=$(cygpath -wl "$1")
+    PythonFile=$(cygpath -wl "$script")
 else
     # otherwise, don't
-    PythonFile=$1
+    PythonFile=$script
 fi
 
-python ${PythonFile}
+echo python ${PythonFile} $*
+python ${PythonFile} $*
