e4e96c750d2591d56b6f5c7880c03f6dddd9f4f7
Richard Plevin <rich@plevin.com>
2016-03-30 11:57:25 -0700

    Converted to a subcommand.

--- pygcam/landProtection.py
+++ pygcam/landProtection.py
@@ -10,13 +10,13 @@ import sys
 import os
 from lxml import etree as ET
 import copy
-#import argparse
 
 from .common import mkdirs, flatten
 from .query import GCAM_32_REGIONS
-from .config import readConfigFiles, getParam, DEFAULT_SECTION
-from .error import PygcamException
+from .config import getParam
+from .error import FileFormatError, CommandlineError, PygcamException
 from .log import getLogger
+from .subcommand import SubcommandABC
 
 _logger = getLogger(__name__)
 
@@ -36,7 +36,6 @@ def _makeRegionXpath(regions):
     patterns = map(lambda s: "@name='%s'" % s, regions)
     regionPattern = ' or '.join(patterns)
     xpath = "//region[%s]" % regionPattern
-    # print xpath
     return xpath
 
 def _makeLandClassXpath(landClasses, protected=False):
@@ -47,7 +46,6 @@ def _makeLandClassXpath(landClasses, protected=False):
     patterns = map(lambda s: "starts-with(@name, '%s%s')" % (prefix, s), landClasses)
     landPattern = ' or '.join(patterns)
     xpath = ".//UnmanagedLandLeaf[%s]" % landPattern
-    # print xpath
     return xpath
 
 def findChildren(node, tag, cls=None):
@@ -88,6 +86,7 @@ class XMLFile(object):
     :param raiseError: if True, raise an error if validation fails
     :param rootClass: optional root class, which is instantiated for the parsed
       tree and stored internally
+    :raises: FileFormatError
     """
     def __init__(self, xmlFile, schemaFile=None, raiseError=True, rootClass=None):
         parser = ET.XMLParser(remove_blank_text=True)
@@ -103,7 +102,7 @@ class XMLFile(object):
             try:
                 schema.assertValid(self.tree)
             except ET.DocumentInvalid as e:
-                raise PygcamException("Validation of '%s'\n  using schema '%s' failed:\n  %s" % (xmlFile, schemaFile, e))
+                raise FileFormatError("Validation of '%s'\n  using schema '%s' failed:\n  %s" % (xmlFile, schemaFile, e))
         else:
             return schema.validate(self.tree)
 
@@ -117,7 +116,7 @@ class LandProtection(object):
     Stores the application's representation of the parsed XML file describing land
     protection scenarios.
 
-    :param node: an lxml.etree.Element representing the top-level ``<landProtection>`` node
+    :param node: an ``lxml.etree.Element`` representing the top-level ``<landProtection>`` node
     """
     def __init__(self, node):
         self.groups    = findChildren(node, 'group')
@@ -143,7 +142,7 @@ class LandProtection(object):
 
         scenario = Scenario.getScenario(scenarioName)
         if not scenario:
-            raise PygcamException("Scenario '%s' was not found" % scenarioName)
+            raise FileFormatError("Scenario '%s' was not found" % scenarioName)
 
         # Iterate over all definitions for this scenario, applying the protections
         # incrementally to the tree representing the XML file that was read in.
@@ -154,28 +153,15 @@ class LandProtection(object):
 
         if backup:
             try:
-                # This is mainly for testing to ensure we're not clobbering reference files. Ignore errors.
-                os.rename(outfile, outfile + '~')
-            except:
-                pass
+                # Ensure we're not clobbering reference files.
+                backupFile = outfile + '~'
+                os.rename(outfile, backupFile)
+            except Exception as e:
+                PygcamException('Failed to create backup file "%s": %s', backupFile, e)
 
-        print "Writing '%s'..." % outfile,
+        _logger.info("Writing '%s'...", outfile)
         tree.write(outfile, xml_declaration=True, pretty_print=True)
-        print 'done.'
 
-    # @staticmethod
-    # def validateXML(doc, raiseError=True):
-    #     '''
-    #     Validate a parsed project.xml file
-    #     '''
-    #     schemaFile = os.path.join(os.path.dirname(__file__), 'pygcam', 'etc', 'protection-schema.xsd')
-    #     schemaDoc = ET.parse(schemaFile)
-    #     schema = ET.XMLSchema(schemaDoc)
-    #
-    #     if raiseError:
-    #         schema.assertValid(doc)
-    #     else:
-    #         return schema.validate(doc)
 
 class Group(object):
     Instances = {}
@@ -249,7 +235,7 @@ class Scenario(object):
         self.protRegDict = protRegDict = {}       # allows regions to override groups
 
         for protReg in self.protRegs:
-            # print "Processing protectedRegion '%s'" % protReg.name
+            _logger.debug("Processing protectedRegion '%s'", protReg.name)
             regions = protReg.expandNames()
 
             # print protReg.name, ' => ', regions
@@ -299,7 +285,7 @@ def createProtected(tree, fraction, landClasses=UnmanagedLandClasses, regions=No
             node = nodes[0]
             regNodes = list(node.iterancestors(tag='region'))
             region = regNodes[0].get('name')
-            raise PygcamException('Error: Land class %s is already protected in region %s' % (node.tag, region))
+            raise FileFormatError('Error: Land class %s is already protected in region %s' % (node.tag, region))
 
         nodes = landRoot.xpath(unmgdXpath)
 
@@ -341,90 +327,6 @@ def protectLand(infile, outfile, fraction, landClasses=UnmanagedLandClasses, reg
 
 DefaultTemplate = 'prot_{fraction}_{filename}'
 
-#
-# def argParser(program='', version=''):
-#     parser = argparse.ArgumentParser(
-#         prog=program,
-#         description='''Generate versions of GCAM's land_input XML files that protect a
-# given fraction of land of the given land types in the given regions. The script can be
-# run multiple times on the same file to apply different percentage protection to
-# distinct regions or land classes. The script detects if you attempt to protect
-# already-protected land class and region combinations, as this fails in GCAM.''')
-#
-#     parser.add_argument('-b', '--backup', action='store_true',
-#                         help='''Make a copy of the output file, if it exists (with an added ~ after
-#                         filename) before writing new output.''')
-#
-#     parser.add_argument('-c', '--configSection', type=str, default=DEFAULT_SECTION,
-#                         help='''The name of the section in the config file to read from.
-#                         Defaults to %s''' % DEFAULT_SECTION)
-#
-#     parser.add_argument('-f', '--fraction', type=float, default=None,
-#                         help='''The fraction of land in the given land classes to protect. (Required)''')
-#
-#     parser.add_argument('-i', '--inFile', action='append',
-#                         help='''One or more input files to process. Use separate -i flags for each file.''')
-#
-#     parser.add_argument('--inPlace', action='store_true',
-#                         help='''Edit the file in place. This must be given explicitly, to avoid overwriting
-#                         files by mistake.''')
-#
-#     parser.add_argument('-l', '--landClasses', action='append',
-#                         help='''The land class or classes to protect in the given regions. Multiple,
-#                         comma-delimited land types can be given in a single argument, or the -l flag can
-#                         be repeated to indicate additional land classes. By default, all unmanaged land
-#                         classes are protected. Allowed land classes are %s''' % UnmanagedLandClasses)
-#
-#     parser.add_argument('-m', '--mkdir', action='store_true',
-#                         help='''Make the output dir if necessary.''')
-#
-#     parser.add_argument('-o', '--outDir', type=str, default='.',
-#                         help='''The directory into which to write the modified files. Default is current directory.''')
-#
-#     parser.add_argument('-t', '--template', type=str, default=DefaultTemplate,
-#                         help='''Specify a template to use for output filenames. The keywords {fraction}, {filename},
-#                         {regions}, and {classes} (with surrounding curly braces) are replaced by the following values
-#                         and used to form the name of the output files, written to the given output directory.
-#                         fraction: 100 times the given fraction (i.e., int(fraction * 100));
-#                         filename: the name of the input file being processed (e.g., land_input_2.xml or land_input_3.xml);
-#                         basename: the portion of the input filename prior to the extension (i.e., before '.xml');
-#                         regions: the given regions, separated by '-', or the word 'global' if no regions are specified;
-#                         classes: the given land classes, separated by '-', or the word 'unmanaged' if no land classes
-#                         are specified. The default pattern is "%s".''' % DefaultTemplate)
-#
-#     parser.add_argument('-r', '--regions', action='append',
-#                         help='''The region or regions for which to protect land. Multiple, comma-delimited
-#                         regions can be given in a single argument, or the -r flag can be repeated to indicate
-#                         additional regions. By default, all regions are protected.''')
-#
-#     parser.add_argument('-s', '--scenario', default=None,
-#                         help='''The name of a land-protection scenario defined in the file given by the --scenarioFile
-#                         argument or it's default value.''')
-#
-#     parser.add_argument('-S', '--scenarioFile', default=None,
-#                         help='''An XML file defining land-protection scenarios. Default is the value
-#                         of configuration file parameter GCAM.LandProtectionXmlFile.''')
-#
-#     parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + version)
-#
-#     parser.add_argument('-w', '--workspace', type=str, default=None,
-#                         help='''Specify the path to the GCAM workspace to use. If input files are not identified
-#                         explicitly, the files in {workspace}/input/gcam-data-system/xml/aglu-xml/land_input_{2,3}.xml
-#                         are used as inputs. Default is value of configuration parameter GCAM.ReferenceWorkspace.''')
-#
-#     return parser
-#
-# def parseArgs(program, version, args=None):
-#     """
-#     Allows calling the arg parser programmatically.
-#
-#     :param args: The parameter list to parse.
-#     :return: populated Namespace instance
-#     """
-#     parser = argParser(program, version)
-#     args = parser.parse_args(args=args)
-#     return args
-
 
 def driver(args):
 
@@ -435,32 +337,27 @@ def driver(args):
     scenarioFile = args.scenarioFile or getParam('GCAM.LandProtectionXmlFile')
     scenarioName = args.scenario
     outDir    = args.outDir
-    inFiles   = args.inFile
-    workspace = args.workspace or getParam('GCAM.ReferenceWorkspace')
+    workspace = args.workspace or getParam('GCAM.RefWorkspace')
     template  = args.template
     inPlace   = args.inPlace
     backup    = args.backup
 
-    if not inFiles and not workspace:
-        raise PygcamException('Must specify either inFiles or workspace')
+    if not workspace:
+        raise CommandlineError('Workspace must be identified in command-line or config variable GCAM.RefWorkspace')
 
-    if workspace:
-        if inFiles:
-            print "Workspace is defined; ignoring inFiles"
-
-        # compute equivalent 'inFiles' arguments for loop below
-        filenames = ['land_input_2.xml', 'land_input_3.xml']
-        xmlDir = os.path.join(workspace, 'input', 'gcam-data-system', 'xml', 'aglu-xml')
-        inFiles = map(lambda filename: os.path.join(xmlDir, filename), filenames)
+    landXmlFiles = ['land_input_2.xml', 'land_input_3.xml']
+    xmlDir = os.path.join(workspace, 'input', 'gcam-data-system', 'xml', 'aglu-xml')
+    inFiles = map(lambda filename: os.path.join(xmlDir, filename), landXmlFiles)
 
     if args.mkdir:
         mkdirs(outDir)
 
     if scenarioName:
         if not scenarioFile:
-            raise PygcamException('A scenario file was not identified')
+            raise CommandlineError('Scenario "%s" was specified, but a scenario file was not identified',
+                                   scenarioName)
 
-        print "Land-protection scenario '%s'" % scenarioName
+        _logger.debug("Land-protection scenario '%s'", scenarioName)
 
         schemaFile = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'pygcam', 'etc', 'protection-schema.xsd')
         xmlFile = XMLFile(scenarioFile, schemaFile=schemaFile, rootClass=LandProtection)
@@ -471,7 +368,7 @@ def driver(args):
 
             # check that we're not clobbering the input file
             if not inPlace and os.path.lexists(outFile) and os.path.samefile(inFile, outFile):
-                raise PygcamException("Attempted to overwrite '%s' but --inPlace was not specified." % inFile)
+                raise CommandlineError("Attempted to overwrite '%s' but --inPlace was not specified." % inFile)
 
             landProtection.protectLand(inFile, outFile, scenarioName, backup=backup)
 
@@ -479,7 +376,7 @@ def driver(args):
 
     fraction = args.fraction
     if fraction is None:
-        raise PygcamException('If not using protection scenarios, fraction must be provided')
+        raise CommandlineError('If not using protection scenarios, fraction must be provided')
 
     fraction = float(fraction)
     templateDict = {'fraction' : str(int(fraction * 100)),
@@ -493,12 +390,94 @@ def driver(args):
 
         outFile = template.format(**templateDict)
         outPath = os.path.join(outDir, outFile)
-        if Verbose:
-            print "protectLand(%s, %s, %0.2f, %s, %s)" % (path, outFile, fraction, landClasses, regions)
-        protectLand(path, outPath, fraction, landClasses=landClasses, regions=regions) #, template=template)
+        _logger.debug("protectLand(%s, %s, %0.2f, %s, %s)", path, outFile, fraction, landClasses, regions)
+        protectLand(path, outPath, fraction, landClasses=landClasses, regions=regions)
 
+class ProtectLandCommand(SubcommandABC):
+    def __init__(self, subparsers):
+        kwargs = {'help': '''Generate versions of GCAM's land_input XML files that protect a
+                given fraction of land of the given land types in the given regions. The script can
+                be run multiple times on the same file to apply different percentage protection to
+                distinct regions or land classes. The script detects if you attempt to protect
+                already-protected land class and region combinations, as this fails in GCAM.'''}
 
-# def main(program, version):
-#     args = parseArgs(program, version)
-#     readConfigFiles(args.configSection)
-#     driver(args)
+        super(ProtectLandCommand, self).__init__('protect', subparsers, kwargs)
+
+    def addArgs(self, parser):
+        parser.add_argument('-b', '--backup', action='store_true',
+                            help='''Make a copy of the output file, if it exists (with an added ~ after
+                            filename) before writing new output. This option is ignored if a scenario
+                            file is specified.''')
+
+        parser.add_argument('-f', '--fraction', type=float, default=None,
+                            help='''The fraction of land in the given land classes to protect. (Required,
+                            unless a scenario file is specified, in which case this option is ignored.)''')
+
+        parser.add_argument('--inPlace', action='store_true',
+                            help='''Edit the file in place. This must be given explicitly, to avoid overwriting
+                            files by mistake.''')
+
+        parser.add_argument('-l', '--landClasses', action='append',
+                            help='''The land class or classes to protect in the given regions. Multiple,
+                            comma-delimited land types can be given in a single argument, or the -l flag can
+                            be repeated to indicate additional land classes. By default, all unmanaged land
+                            classes are protected. Allowed land classes are %s.
+                            This option is ignored if a scenario file is specified. ''' % UnmanagedLandClasses)
+
+        parser.add_argument('-m', '--mkdir', action='store_true',
+                            help='''Make the output dir if necessary.''')
+
+        parser.add_argument('-o', '--outDir', type=str, default='.',
+                            help='''The directory into which to write the modified files.
+                                    Default is current directory.''')
+
+        parser.add_argument('-t', '--template', type=str, default=DefaultTemplate,
+                            help='''Specify a template to use for output filenames. The keywords {fraction}, {filename},
+                            {regions}, and {classes} (with surrounding curly braces) are replaced by the following values
+                            and used to form the name of the output files, written to the given output directory.
+                            fraction: 100 times the given fraction (i.e., int(fraction * 100));
+                            filename: the name of the input file being processed (e.g., land_input_2.xml or land_input_3.xml);
+                            basename: the portion of the input filename prior to the extension (i.e., before '.xml');
+                            regions: the given regions, separated by '-', or the word 'global' if no regions are specified;
+                            classes: the given land classes, separated by '-', or the word 'unmanaged' if no land classes
+                            are specified. The default pattern is "%s".
+                            This option is ignored if a scenario file is specified.''' % DefaultTemplate)
+
+        parser.add_argument('-r', '--regions', action='append',
+                            help='''The region or regions for which to protect land. Multiple, comma-delimited
+                            regions can be given in a single argument, or the -r flag can be repeated to indicate
+                            additional regions. By default, all regions are protected.
+                            This option is ignored if a scenario file is specified.''')
+
+        parser.add_argument('-s', '--scenario', default=None,
+                            help='''The name of a land-protection scenario defined in the file given by the --scenarioFile
+                            argument or it's default value.''')
+
+        parser.add_argument('-S', '--scenarioFile', default=None,
+                            help='''An XML file defining land-protection scenarios. Default is the value
+                            of configuration file parameter GCAM.LandProtectionXmlFile.''')
+
+        parser.add_argument('-V', '--version', action='version', version='%(prog)s ' + VERSION)
+
+        parser.add_argument('-w', '--workspace', type=str, default=None,
+                            help='''Specify the path to the GCAM workspace to use. The files in
+                            {workspace}/input/gcam-data-system/xml/aglu-xml/land_input_{2,3}.xml
+                            are used as inputs. Default is value of configuration parameter
+                            GCAM.RefWorkspace.''')
+
+        return parser
+
+    def run(self, args, tool):
+        driver(args)
+
+    #
+    # def parseArgs(program, version, args=None):
+    #     """
+    #     Allows calling the arg parser programmatically.
+    #
+    #     :param args: The parameter list to parse.
+    #     :return: populated Namespace instance
+    #     """
+    #     parser = argParser(program, version)
+    #     args = parser.parse_args(args=args)
+    #     return args
