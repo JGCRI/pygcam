9906bf1931a94ee31e7f3f682395cc7f45be2ea2
Richard Plevin <rich@plevin.com>
2016-05-25 12:09:07 -0700

    Normalized unit conversion variable names in prep for use as symbolic "multipliers" for chart sub-command.

--- pygcam/unitConversion.py
+++ pygcam/unitConversion.py
@@ -3,93 +3,168 @@
 #
 # Pythonized from gcam-data-system/_common/assumptions/unit_conversions.R
 #
-ones_mil = 1e-6
-ones_thous = 1e-3
-thous_mil = 1e-3
-mil_bil = 1e-3
-bil_mil = 1e3
-mil_thous = 1e3
-bil_thous = 1e6
-thous_bil = 1e-6
-C_CO2 = 44/12
-CO2_C = 12/44
 
-# dollar conversions
-USD_1990_2005 = 1.383
-USD_1990_2007 = 1.470
-USD_1990_2010 = 1.510
-USD_1990_1975 = 0.4649
-USD_1996_1975 = 0.4049
-USD_1997_1975 = 0.3983
-USD_1998_1975 = 0.3939
-USD_1999_1975 = 0.3883
-USD_2000_1975 = 0.380
-USD_2001_1975 = 0.3711
-USD_2002_1975 = 0.3647
-USD_2003_1975 = 0.3571
-USD_2004_1975 = 0.3472
-USD_2005_1975 = 0.3362
-USD_2006_1975 = 0.3257
-USD_2007_1975 = 0.317
-USD_2008_1975 = 0.3104
-USD_2009_1975 = 0.3104
+# basic SI unit relationships
+k_to_ones = 1e3
+ones_to_k = 1e-3
+
+M_to_ones = 1e6
+ones_to_M = 1e-6
+
+M_to_k = 1e3
+k_to_M = 1e-3
+
+G_to_ones = 1e9
+ones_to_G = 1e-9
+
+G_to_k = 1e6
+k_to_G = 1e-6
+
+G_to_M = 1e3
+M_to_G = 1e-3
+
+P_to_M = 1e9
+M_to_P = 1e-9
+
+P_to_G = 1e6
+G_to_P = 1e-6
+
+P_to_T = 1e3
+T_to_P = 1e-3
+
+E_to_G = 1e9
+G_to_E = 1e-9
+
+# Carbon <--> CO2
+C_to_CO2 = 44/12
+CO2_to_C = 12/44
 
 # mass conversions
-kg_Mt = 1e-9
-t_Mt = 1e-6
-kt_Mt = 1e-3
-Pg_Tg = 1000
-t_kg = 1e3
-kg_t = 1e-3
-g_kg = 1e-3
-t_metric_short = 1000/908
+kg_to_g = k_to_ones
+g_to_kg = ones_to_k
+
+Mg_to_kg = M_to_k
+kg_to_Mg = k_to_M
+
+Mg_to_ton = 1000/908
+ton_to_Mg = 0.908
+
+Mt_to_ton = Mg_to_ton   # aliases
+ton_to_Mt = ton_to_Mg
+
+Pg_to_Tg = P_to_T
+Tg_to_Pg = T_to_P
 
 # calorie conversions
-Mcal_Pcal = 1e-9
-Pcal_Gcal = 1e6
-Pcal_Mcal = 1e9
+Pcal_to_Mcal = P_to_M
+Mcal_to_Pcal = M_to_P
+
+Pcal_to_Gcal = P_to_G
+Gcal_to_Pcal = G_to_P
 
 # volume conversions
-m3_bm3 = 1e-9
-Mm3_km3 = 1e-3
+m3_to_Gm3 = 1e-9
+m3_to_bm3 = m3_to_Gm3
+
+km3_to_Mm3 = 1e3
+Mm3_to_km3 = 1e-3
 
 # area conversions
-Ha_bm2 = 1e-5
-Ha_m2 = 1e4
-m2_acre = 1/4046.85
-km2_bm2 = 1e-3
-bm2_m2 = 1e9
-milft2_bm2 = 0.0929/1e3     # million square feet to billion square meters
-milft2_m2 = 0.0929 * 1e6
-ft2_bm2 = 0.0929/1e9        # square feet to billion square meters
-ft2_m2 = 0.0929
+bm2_to_ha = 1e5
+ha_to_bm2 = 1e-5
+
+ha_to_m2 = 1e4
+m2_to_ha = 1e-4
+
+acre_to_m2 = 4046.85
+m2_to_acre = 1/acre_to_m2
+
+Gm2_to_km2 = G_to_k
+bm2_to_km2 = G_to_k
+km2_to_Gm2 = k_to_G
+km2_to_bm2 = k_to_G
+
+bm2_to_m2 = G_to_ones
+Gm2_to_m2 = G_to_ones
+
+ft2_to_m2 = 0.0929
+Mft2_to_m2 = ft2_to_m2 * M_to_ones
+
+Mft2_to_Gm2 = ft2_to_m2 * M_to_G    # million square feet to billion square meters
+Mft2_to_bm2 = Mft2_to_Gm2
+
+ft2_to_Gm2 = ft2_to_m2 * ones_to_G  # square feet to billion square meters
+ft2_to_bm2 = ft2_to_Gm2
 
 # yield conversions
-tha_kgm2 = 0.1
+Mg_per_ha_to_kg_per_m2 = M_to_k / ha_to_m2
 
-# cotton seed and lint conversion
-cotton_lint = 0.4
+# time
+year_to_day = 365.25
+day_to_year = 1 / year_to_day
+
+day_to_hour = 24
+hour_to_day = 1 / day_to_hour
+year_to_hour = year_to_day * day_to_hour
 
 # energy unit conversions
-kbbl_bbl = 1000
-bbl_tonne_RFO = 1 / 6.66
-bbl_tonne_distillate = 1 / 7.46
-days_year = 1 / 365.25
-year_hours = 8766
-tonne_GJ_RFO = 40.87
-tonne_GJ_distillate = 42.91
-GJ_EJ = 1e-9
-kwh_GJ = 0.0036
-TWh_EJ = 3.6e-3
-GWh_EJ = 3.6e-6
-MWh_EJ = 3.6e-9
-MWh_GJ = 3.6
-MJ_btu = 947.777
-EJ_GJ = 1e9
-btu_kJ = 1.0551
+kbbl_to_bbl = k_to_ones
+bbl_to_tonne_RFO = 1 / 6.66
+bbl_to_tonne_distillate = 1 / 7.46
 
-# from billion barrels a day to EJ per year
-bbld_EJyr = 6.119 * 365.25 * 1e-3
+tonne_to_GJ_RFO = 40.87
+tonne_to_GJ_distillate = 42.91
 
-Tbtu_EJ = 0.0010551         # trillion btu to EJ
-kbtu_EJ = 1.0551e-12        # thousand btu to EJ
+GJ_to_EJ = G_to_E
+EJ_to_GJ = E_to_G
+
+kWh_to_MJ = 3.6
+MWh_to_GJ = kWh_to_MJ
+
+kWh_to_GJ = 0.0036
+kWh_to_GJ = kWh_to_MJ * M_to_G
+
+MWh_to_EJ = 3.6e-9
+GWh_to_EJ = 3.6e-6
+TWh_to_EJ = 3.6e-3
+
+btu_to_kJ = 1.0551
+kJ_to_btu = 1 / btu_to_kJ
+
+MJ_to_btu = kJ_to_btu * M_to_k
+btu_to_MJ = 1 / MJ_to_btu
+
+MMbtu_to_MJ = M_to_ones * btu_to_MJ
+MJ_to_MMbtu = 1 / MMbtu_to_MJ
+
+MJ_to_MMBtu = MJ_to_MMbtu   # alternate spelling
+MMBtu_to_MJ = MMbtu_to_MJ
+
+quad_to_EJ = btu_to_kJ
+EJ_to_quad = 1/quad_to_EJ
+
+Tbtu_to_EJ = 0.0010551         # trillion btu to EJ
+kbtu_to_EJ = 1.0551e-12        # thousand btu to EJ
+
+# from billion barrels a day to EJ per year (comment on original was wrong: bbl = barrel, not billion bbl)
+# bbl_per_day_to_EJ_per_yr = 6.119 * 365.25 * 1e-3
+
+# dollar inflation/deflation
+USD_1990_to_2005 = 1.383
+USD_1990_to_2007 = 1.470
+USD_1990_to_2010 = 1.510
+USD_1990_to_1975 = 0.4649
+USD_1996_to_1975 = 0.4049
+USD_1997_to_1975 = 0.3983
+USD_1998_to_1975 = 0.3939
+USD_1999_to_1975 = 0.3883
+USD_2000_to_1975 = 0.380
+USD_2001_to_1975 = 0.3711
+USD_2002_to_1975 = 0.3647
+USD_2003_to_1975 = 0.3571
+USD_2004_to_1975 = 0.3472
+USD_2005_to_1975 = 0.3362
+USD_2006_to_1975 = 0.3257
+USD_2007_to_1975 = 0.317
+USD_2008_to_1975 = 0.3104
+USD_2009_to_1975 = 0.3104
