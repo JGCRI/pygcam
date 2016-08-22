357eda8a8ae96c8198efb09de127672f2f505087
Richard Plevin <rich@plevin.com>
2016-08-22 10:38:13 -0700

    Create initial scenarioSetup.py

--- pygcam/scenarioSetup.py
+++ pygcam/scenarioSetup.py
@@ -1,49 +1,25 @@
 '''
-.. Created on: 5/11/16
+.. Created on: 8/21/16
 .. codeauthor:: Rich Plevin <rich@plevin.com>
 
 .. Copyright (c) 2016 Richard Plevin
    See the https://opensource.org/licenses/MIT for license details.
 '''
-from .error import PygcamException
-from .utils import XMLFile, getBooleanXML, resourceStream
+from pygcam.utils import XMLFile, getBooleanXML, resourceStream
 
 #
-# Classes to parse queryFiles and the <queries> element of project.xml
-# (see pygcam/etc/queryFile-schema.xsd). These are in a separate file
-# for sharing between query.py and project.py
+# Classes to parse "simple" scenario setup files.
+# (See pygcam/etc/scenarioSetup-schema.xsd).
 #
-class Query(object):
-    def __init__(self, node, defaultMap):
-        self.name = node.get('name')
-        self.delete = getBooleanXML(node.get('delete', '1'))
-        self.useDefault = useDefault = getBooleanXML(node.get('useDefault', '1'))
-
-        # see if the user provided the attribute, or we defaulted to 1
-        explicitUseDefault = node.get('useDefault', None) and useDefault
-
-        # Create a list of tuples with (mapName, level) where level may be None
-        rewriters = node.findall('rewriter')
-        self.rewriters = map(lambda obj: (obj.get('name'), obj.get('level')), rewriters)
-
-        # We add the default map in two cases: (i) user specified some rewriters and explicitly
-        # set useDefault="1", or (ii) there are no rewriters and useDefault has not been set to "0".
-        if defaultMap and ((rewriters and explicitUseDefault) or (not rewriters and useDefault)):
-            self.rewriters.append((defaultMap, None))
-
-    def getName(self):
-        return self.name
-
-class QueryFile(object):
+class ScenarioSetup(object):
     def __init__(self, node):
-        defaultMap = self.defaultMap = node.get('defaultMap', None)
+        self.name = node.get('name')
 
-        nodes = node.findall('query')
-        self.queries = [Query(node, defaultMap) for node in nodes]
+        self.iterators  = map(Iterator, node.findall('vars/iterator'))
+        self.scenGroups = map(ScenarioGroup, node.findall('scenarioGroup'))
 
-    def queryNames(self):
-        names = map(Query.getName, self.queries)
-        return names
+        # self.delete = getBooleanXML(node.get('delete', '1'))
+        # self.useDefault = useDefault = getBooleanXML(node.get('useDefault', '1'))
 
     @classmethod
     def parse(cls, filename):
@@ -52,54 +28,79 @@ class QueryFile(object):
         :param filename: (str) the name of the XML file to read
         :return: a QueryFile instance.
         """
-        schemaStream = resourceStream('etc/queryFile-schema.xsd')
+        schemaStream = resourceStream('etc/scenarioSetup-schema.xsd')
         xmlFile = XMLFile(filename, schemaFile=schemaStream)
         return cls(xmlFile.tree.getroot())
 
-#
-# Classes to parse rewriteSets.xml (see pygcam/etc/rewriteSets-schema.xsd)
-#
-class Rewrite(object):
+    def run(self, name, scenario, group=None):
+        """
+        Run the setup with the given `name`.
+
+        :param name: (str) the name of the ScenarioSetup
+        :param scenario: (str) the name of the scenario to run
+        :param group: (str) name of scenario group. If not provided,
+           the default group will be used, or the only group if there
+           is only one.
+        :return: none
+        """
+        pass
+
+class Iterator(object):
     def __init__(self, node):
-        self.From = node.get('from')    # 'from' is a keyword...
-        self.to   = node.get('to')
-        self.byAEZ = getBooleanXML(node.get('byAEZ', '0'))
+        self.name = node.get('name')
+        self.type = numericType = int if node.get('type') == 'int' else float
+        self.min  = numericType(node.get('min'))
+        self.max  = numericType(node.get('max'))
+        self.incr = numericType(node.get('incr', 1))
+
+        iterators  = map(Iterator, node.findall('vars/iterators'))
+        scenGroups = map(ScenarioGroup, node.findall('scenarioGroup'))
 
     def __str__(self):
-        return "<Rewrite from='%s' to='%s' byAEZ='%s'>" % (self.From, self.to, self.byAEZ)
+        return "<iterator name='%s' type='%s' min='%s' max='%s' incr='%s'/>" % \
+               (self.name, self.type, self.min, self.max, self.incr)
 
-
-class RewriteSet(object):
+class ScenarioGroup(object):
     def __init__(self, node):
-        self.name  = node.get('name')
-        self.level = node.get('level')
-        self.byAEZ = getBooleanXML(node.get('byAEZ', '0'))
-        self.appendValues = getBooleanXML(node.get('append-values', '0'))
-        self.rewrites = map(Rewrite, node.findall('rewrite'))
+        self.name = node.get('name')
+        self.useGroupDir = getBooleanXML(node.get('useGroupDir', 0))
+        self.iteratorName = node.get('iterator')
+
+        self.scenarios = map(Scenario, node.findall('scenario'))
+
+class Scenario(object):
+    def __init__(self, node):
+        self.name = node.get('name')
+        self.active   = getBooleanXML(node.get('baseline', 0))
+        self.baseline = getBooleanXML(node.get('active', 1))
+        self.iteratorName = node.get('iterator')
+
+        self.actions = map(ConfigAction, node.findall('config'))
+        self.generators = map(Generator, node.findall('generator'))
 
     def __str__(self):
-        return "<RewriteSet name='%s' level='%s' byAEZ='%s' append-values='%s'>" % \
-               (self.name, self.level, self.byAEZ, self.appendValues)
+        return "<scenario name='%s'>" % self.name
 
-class RewriteSetParser(object):
-    def __init__(self, node, filename):
-        rewriteSets = map(RewriteSet, node.findall('rewriteSet'))
-        self.rewriteSets = {obj.name : obj for obj in rewriteSets}
-        self.filename = filename # for error messages only
+class ConfigAction(object):
+    def __init__(self, node):
+        self.name = node.get('name')
+        self.action = node.get('action')
+        self.dir = node.get('dir', '')
+        self.xmlFile = node.text
 
-    def getRewriteSet(self, name):
-        try:
-            return self.rewriteSets[name]
-        except KeyError:
-            raise PygcamException('RewriteSet "%s" not found in file "%s"' % (name, self.filename))
+    def __str__(self):
+        return "<config name='%s' action='%s' dir='%s'>%s</config>" % \
+               (self.name, self.action, self.dir, self.xmlFile)
 
-    @classmethod
-    def parse(cls, filename):
-        """
-        Parse an XML file holding a list of query result rewrites.
-        :param filename: (str) the name of the XML file to read
-        :return: a list of RewriteSet instances
-        """
-        schemaStream = resourceStream('etc/rewriteSets-schema.xsd')
-        xmlFile = XMLFile(filename, schemaFile=schemaStream)
-        return cls(xmlFile.tree.getroot(), filename)
+class Generator(object):
+    def __init__(self, node):
+        self.name = node.get('name')
+        self.content = node.text
+
+    def __str__(self):
+        return "<generator name='%s'>%s</generator>" % (self.name, self.content)
+
+
+if __name__ == '__main__':
+    obj = ScenarioSetup.parse('etc/scenarioSetup-example.xml')
+    print obj
