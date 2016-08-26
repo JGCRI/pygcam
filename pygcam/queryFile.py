1c9ac2572b20c2a4a96ef5d0e7791498fa7a213d
Richard Plevin <rich@plevin.com>
2016-08-25 21:45:31 -0700

    More header/copyright updates

--- pygcam/queryFile.py
+++ pygcam/queryFile.py
@@ -1,7 +1,5 @@
 '''
 .. Created on: 5/11/16
-.. codeauthor:: Rich Plevin <rich@plevin.com>
-
 .. Copyright (c) 2016 Richard Plevin
    See the https://opensource.org/licenses/MIT for license details.
 '''
