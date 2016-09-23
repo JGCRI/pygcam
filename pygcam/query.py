b726a386555c5d2a34fc1337b86bd75418f9440a
Richard Plevin <rich@plevin.com>
2016-09-23 12:43:51 -0700

    fixed prequery handling

--- pygcam/query.py
+++ pygcam/query.py
@@ -940,7 +940,7 @@ def queryMain(args):
     # Post-GCAM queries are not possible when using in-memory database.
     # The 'prequery' step writes the XMLDBDriver.properties file used
     # by GCAM to query the in-memory database before exiting.
-    if version <= 4.2:
+    if prequery and version <= 4.2:
         _logger.info('Skipping pre-query step for GCAM %s', version)
         return
 
